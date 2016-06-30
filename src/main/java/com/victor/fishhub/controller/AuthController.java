dae5e7a75344471b63210a80b0c597127d83fbdf
BillyFreeman <victor.freeman86@gmail.com>
2016-07-01 01:06:57 +0300

    add Spring Security config file and dependencies. Add securityFilterChain and security context declaration to web.xml

--- src/main/java/com/victor/fishhub/controller/AuthController.java
+++ src/main/java/com/victor/fishhub/controller/AuthController.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.victor.fishhub.controller;
 
-/**
- *
- * @author Віктор
- */
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class AuthController {
     
+  
+
 }
