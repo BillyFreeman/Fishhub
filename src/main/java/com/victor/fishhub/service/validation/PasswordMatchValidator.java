e331c9cc1eb7a38ccaf4f01bc737f074cc402200
BillyFreeman <victor.freeman86@gmail.com>
2016-07-05 18:43:48 +0300

    PasswordMatchValidator implemented

--- src/main/java/com/victor/fishhub/service/validation/PasswordMatchValidator.java
+++ src/main/java/com/victor/fishhub/service/validation/PasswordMatchValidator.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.victor.fishhub.service.validation;
 
+import com.victor.fishhub.service.auth.dto.RegDTO;
 import com.victor.fishhub.service.validation.annotation.PasswordMatch;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-/**
- *
- * @author Віктор
- */
 public class PasswordMatchValidator implements ConstraintValidator<PasswordMatch, Object>{
 
     @Override
     public void initialize(PasswordMatch constraintAnnotation) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(value.getClass() != RegDTO.class){
+            return false;
+        }
+        RegDTO regDTO = (RegDTO) value;
+        return regDTO.getPassword().equals(regDTO.getConfirmPassword());
     }
     
 }
